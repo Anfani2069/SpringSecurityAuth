1225b06a29b9ec5a44da380f739b843bdc639948
Anfani2069 <60386513+Anfani2069@users.noreply.github.com>
2024-01-26 11:08:33 +0100

    Spring security project

---
+++ src/main/java/com/project/repository/DBUserRepository.java
@@ -0,0 +1,8 @@
+package com.project.repository;
+
+import com.project.model.DBUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DBUserRepository extends JpaRepository<DBUser, Integer> {
+    public DBUser findByUsername(String username);
+}
